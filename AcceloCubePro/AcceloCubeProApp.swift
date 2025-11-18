4ed476a00ce77a1f4c247c2a34624fb30407d3ab
aramburo reyes francisco eduardo <aramburoeduardo972@gmail.com>
2025-11-17 18:22:30 -0800

    Initial Commit

---
+++ AcceloCubePro/AcceloCubeProApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcceloCubeProApp.swift
+//  AcceloCubePro
+//
+//  Created by francisco eduardo aramburo reyes on 17/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AcceloCubeProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
